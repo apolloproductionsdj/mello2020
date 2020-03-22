a371b24747d561478a5fffd96beea156d3e691ed
Mike Torres <apolloproductionsdj@gmail.com>
2020-03-22 18:47:16 +0000

    set element attributes using jQuery

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,6 +1,9 @@
 const $setLogin = $('#login');
 const $setSignUp = $('#signup');
 const $submitButton = $('#submit');
+const $emailInput = $('#email');
+const $passwordInput = $('#password');
+const $message = $('#message');
 
 let authSetting = 'login';
 
@@ -10,7 +13,7 @@ function setAuth(setting) {
   if (authSetting === 'login') {
     $setLogin.addClass('active');
     $setSignUp.removeClass('active');
-    $submitButton.text('Login');
+    $submitButton.text('Log In');
   } else {
     $setSignUp.addClass('active');
     $setLogin.removeClass('active');
@@ -18,5 +21,26 @@ function setAuth(setting) {
   }
 }
 
+function handlFormSubmit(event) {
+  event.preventDefault();
+
+  let email = $emailInput.val().trim();
+  let password = $passwordInput.val().trim();
+
+  if (!email || !password) {
+    displayMessage('Email and password fields cannot be blank.', 'danger');
+    return;
+  }
+
+  console.log(
+    `Email: ${email} Password: ${password} AuthSetting: ${authSetting}`
+  );
+}
+
+function displayMessage(message, type) {
+  $message.text(message).attr('class', type);
+}
+
 $setLogin.on('click', setAuth.bind(null, 'login'));
 $setSignUp.on('click', setAuth.bind(null, 'signup'));
+$submitButton.on('click', handlFormSubmit);
